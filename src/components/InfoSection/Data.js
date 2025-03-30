01f7b08c84e69eb7dc2e7f4c48cee8c81298292a
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2025-03-30 22:32:07 +0100

    Update resume link and introduction

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,20 +1,24 @@
 import aboutmeimg from '../../images/aboutme3.png'
 import Icon2 from '../../images/svg2.svg'
-export const About= {
-    id:'about',
-    topLine: 'About me',
-    headLine: 'Software Engineer x Developer',
-    description: 'Hi readers, my nickname is Boon and I am currently a \
-    senior student at the University of Glasgow— majoring in Software Engineering, Computer Science. \
-    Please feel free to check my Resume and other details below! :)',
-    newline: false,
-    buttonLabel: 'Resume',
-    to:{pathname:'https://drive.google.com/file/d/1nhfgAUE6rK5Xas0dlIk3xT71U2n8ovD5/view?usp=sharing'},
-    target: "_blank",
-    imgStart: false,
-    img: aboutmeimg,
-    alt: "Boondirek Kanjanapongporn",
-}
+export const About = {
+  id: "about",
+  topLine: "About me",
+  headLine: "Software Engineer x Developer",
+  description:
+        "Hi readers, my nickname is Boon, and I am a graduate of \
+    the University of Glasgow with a 1st Class Honours degree in Software Engineering and Computer Science. \
+    Please feel free to check my Resume and other details below! :)",
+  newline: false,
+  buttonLabel: "Resume",
+  to: {
+    pathname:
+      "https://drive.google.com/file/d/1nhfgAUE6rK5Xas0dlIk3xT71U2n8ovD5/view?usp=sharing",
+  },
+  target: "_blank",
+  imgStart: false,
+  img: aboutmeimg,
+  alt: "Boondirek Kanjanapongporn",
+};
 
 export const Skills= {
     id:'skills',
